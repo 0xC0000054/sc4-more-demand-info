bd35fde8ce0fea9fb54a8c4171ad32fe9c1f62b8
Nicholas Hayes <0xC0000054@users.noreply.github.com>
2026-05-25 18:05:45 -0600

    Fix a mix of library names in the file headers

--- src/version.h
+++ src/version.h
@@ -4,18 +4,18 @@
  *
  * Copyright (C) 2024, 2026 Nicholas Hayes
  *
- * sc4-more-building-styles is free software: you can redistribute it and/or
+ * sc4-more-demand-info is free software: you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public License as
  * published by the Free Software Foundation, either version 3 of
  * the License, or (at your option) any later version.
  *
- * sc4-more-building-styles is distributed in the hope that it will be useful,
+ * sc4-more-demand-info is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public License
- * along with sc4-more-building-styles.
+ * along with sc4-more-demand-info.
  * If not, see <http://www.gnu.org/licenses/>.
  */
 
